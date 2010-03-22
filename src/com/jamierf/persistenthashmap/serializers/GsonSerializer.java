162b6464501fe5c8e106719248535b96ff060678
Jamie Furness <jamie@jamierf.co.uk>
2010-03-22 17:01:48 +0000

    Adding license information

--- src/com/jamierf/persistenthashmap/serializers/GsonSerializer.java
+++ src/com/jamierf/persistenthashmap/serializers/GsonSerializer.java
@@ -1,3 +1,11 @@
+/**
+ * 
+ * This file is part of the PersistentHashMap library.
+ * Copyright (C) 2010 George Haney (http://www.georgemh.com)
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 3 (or higher)
+ * 
+ */
+
 package com.jamierf.persistenthashmap.serializers;
 
 import java.io.File;
--- src/com/jamierf/persistenthashmap/serializers/OOSSerializer.java
+++ src/com/jamierf/persistenthashmap/serializers/OOSSerializer.java
@@ -1,3 +1,11 @@
+/**
+ * 
+ * This file is part of the PersistentHashMap library.
+ * Copyright (C) 2010 Jamie Furness (http://www.jamierf.co.uk)
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 3 (or higher)
+ * 
+ */
+
 package com.jamierf.persistenthashmap.serializers;
 
 import java.io.File;
